6b297c9674c6daaff7ea7dee3ef0fab29977f3bc
luke <108032666+kejjjjj@users.noreply.github.com>
2025-06-22 22:47:05 +0300

    debug lines

--- scripting/sc_debug.hpp
+++ scripting/sc_debug.hpp
@@ -4,3 +4,4 @@
 
 using IValues = std::vector<class IValue*>;
 class IValue* WorldDebugString(struct CRuntimeContext* const ctx, IValue* _this, const IValues& args);
+class IValue* WorldDebugLine(struct CRuntimeContext* const ctx, IValue* _this, const IValues& args);
